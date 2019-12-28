143a46715d1da4d07549d8a9779d473e38d1b855
pratikshatamadalge <54092003+pratikshatamadalge@users.noreply.github.com>
2019-12-28 14:18:05 +0530

    Updated: validation of RegisterDTO

--- src/main/java/com/bridgelabz/usermanagement/dto/RegisterDTO.java
+++ src/main/java/com/bridgelabz/usermanagement/dto/RegisterDTO.java
@@ -9,20 +9,33 @@ import lombok.Data;
  */
 @Data
 public class RegisterDTO {
+	@NonNull
 	private String emailId;
+	@NonNull
 	private String firstName;
+	@NonNull
 	private String middleName;
+	@NonNull
 	private String lastName;
+	@NonNull
 	private LocalDate dob;
+	@NonNull
 	private String address;
+	@NonNull
 	private String gender;
+	@NonNull
 	private String country;
+	@NonNull
 	private String mobile;
+	@NonNull
 	private String phoneExt;
-
+	@NonNull
 	private String userName;
+	@NonNull
 	private String password;
+	@NonNull
 	private String confirmPassword;
+	@NonNull
 	private String userRole;
 	private String profilePic;
 
